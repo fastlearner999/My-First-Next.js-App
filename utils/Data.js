e7b302428736e55d62adb330b210eaa1b81ab5ed
Aaron Smith <2021manifest@gmail.com>
2022-07-08 21:56:27 +0100

    added utils folder, added data.js with 5 products

---
+++ utils/Data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+            name: "Toothbrush Cover",
+            slug: "toothbrush cover",
+            category: "oral heath",
+            image: "/images/tbcover.jpg",
+            price: 2,
+            brand: 'think of something',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 20,
+            description: 'A popular toothbrush cover'
+        },
+        {
+            name: "Copper Tongue Scrapper",
+            slug: "Copper Tongue Scrapper",
+            category: "oral heath",
+            image: "/images/ctscrapper.jpg",
+            price: 5,
+            brand: 'think of something',
+            rating: 4.5,
+            numReviews: 3,
+            countInStock: 42,
+            description: 'A popular copper tongue scrapper'
+        },
+        {
+            name: "Lip Balm SheaButter",
+            slug: "Lip Balm SheaButter",
+            category: "oral heath",
+            image: "/images/lipbalm.jpg",
+            price: 3,
+            brand: 'think of something',
+            rating: 4.5,
+            numReviews: 18,
+            countInStock: 60,
+            description: 'A popular lip balm 100% sheabutter'
+        },
+        {
+            name: "Fitted T-shirt",
+            slug: "Fitted T-shirt",
+            category: "shirt",
+            image: "/images/trill.jpg",
+            price: 25,
+            brand: 'Trill-Living',
+            rating: 3.2,
+            numReviews: 45,
+            countInStock: 12,
+            description: 'A popular t-shirt'
+        },
+        {
+            name: "Star-Sign T-shirt",
+            slug: "Star-Sign T-shirt",
+            category: "shirt",
+            image: "/images/starsign.jpg",
+            price: 19.99,
+            brand: 'think of something',
+            rating: 3.5,
+            numReviews: 28,
+            countInStock: 37,
+            description: 'A popular t-shirt'
+        }
+    ]
+}
+
+export default data
